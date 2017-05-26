1ba29bbc3b8841aefcdeb154a3710e6b613ae7c7
kaklakariada <kaklakariada@users.noreply.github.com>
2017-05-26 14:39:58 +0200

    Adapt to new Lambd request handler

--- src/main/java/com/github/kaklakariada/blog/handler/PostCommentHandler.java
+++ src/main/java/com/github/kaklakariada/blog/handler/PostCommentHandler.java
@@ -10,6 +10,8 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.github.kaklakariada.aws.lambda.LambdaRequestHandler;
+import com.github.kaklakariada.aws.lambda.controller.LambdaController;
+import com.github.kaklakariada.aws.lambda.controller.RequestHandlerMethod;
 import com.github.kaklakariada.aws.lambda.request.ApiGatewayRequest;
 import com.github.kaklakariada.blog.model.BlogPostComment;
 import com.github.kaklakariada.blog.model.BlogPostCommentDynamoDb;
@@ -19,32 +21,34 @@ public class PostCommentHandler extends LambdaRequestHandler<BlogPostComment, Vo
 	private static final Logger LOG = LoggerFactory.getLogger(PostCommentHandler.class);
 
 	public PostCommentHandler() {
-		super(BlogPostComment.class);
+		super(new Controller(), BlogPostComment.class, Void.class);
 	}
 
-	@Override
-	public Void handleRequest(ApiGatewayRequest request, BlogPostComment body, Context context) {
-		LOG.info("Request: {}", request);
-		LOG.info("Body: {}", body);
+	private static class Controller implements LambdaController<BlogPostComment, Void> {
+		@RequestHandlerMethod
+		public Void handleRequest(ApiGatewayRequest request, BlogPostComment body, Context context) {
+			LOG.info("Request: {}", request);
+			LOG.info("Body: {}", body);
 
-		final DynamoDBMapper dbMapper = createDbMapper(request);
-		final int blogPostId = 42;
-		final BlogPostCommentDynamoDb entry = new BlogPostCommentDynamoDb(blogPostId, body);
-		dbMapper.save(entry);
-		LOG.info("New db entry: {}", entry);
-		return null;
-	}
+			final DynamoDBMapper dbMapper = createDbMapper(request);
+			final int blogPostId = 42;
+			final BlogPostCommentDynamoDb entry = new BlogPostCommentDynamoDb(blogPostId, body);
+			dbMapper.save(entry);
+			LOG.info("New db entry: {}", entry);
+			return null;
+		}
 
-	private DynamoDBMapper createDbMapper(ApiGatewayRequest request) {
-		final AmazonDynamoDB dynamoDB = AmazonDynamoDBClient.builder().build();
-		final String tableNamePrefix = request.getRequestContext().getStage() + "_";
-		LOG.info("Using table prefix {}", tableNamePrefix);
-		final DynamoDBMapperConfig config = getMapperConfig(tableNamePrefix);
-		return new DynamoDBMapper(dynamoDB, config);
-	}
+		private DynamoDBMapper createDbMapper(ApiGatewayRequest request) {
+			final AmazonDynamoDB dynamoDB = AmazonDynamoDBClient.builder().build();
+			final String tableNamePrefix = request.getRequestContext().getStage() + "_";
+			LOG.info("Using table prefix {}", tableNamePrefix);
+			final DynamoDBMapperConfig config = getMapperConfig(tableNamePrefix);
+			return new DynamoDBMapper(dynamoDB, config);
+		}
 
-	private DynamoDBMapperConfig getMapperConfig(String tableNamePrefix) {
-		return DynamoDBMapperConfig.builder()
-				.withTableNameOverride(TableNameOverride.withTableNamePrefix(tableNamePrefix)).build();
+		private DynamoDBMapperConfig getMapperConfig(String tableNamePrefix) {
+			return DynamoDBMapperConfig.builder()
+					.withTableNameOverride(TableNameOverride.withTableNamePrefix(tableNamePrefix)).build();
+		}
 	}
 }
